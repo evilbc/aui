44608e12a834ef4855d107b48bdcbd633f7ec830
evilbc <pawel.jastrzebski01@gmail.com>
2023-10-24 10:31:33 +0200

    Poprawy na zajęciach

--- lab2/src/main/java/lab2/developer/repository/IDeveloperRepository.java
+++ lab2/src/main/java/lab2/developer/repository/IDeveloperRepository.java
@@ -2,8 +2,10 @@ package lab2.developer.repository;
 
 import lab2.developer.entity.Developer;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
+@Repository
 public interface IDeveloperRepository extends JpaRepository<Developer, UUID> {
 }
